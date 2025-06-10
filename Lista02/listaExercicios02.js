34d079ef1c78da54ae9b53af3f4cfba651531431
Andre Lucho <andretlucho@gmail.com>
2025-06-10 12:11:58 -0300

    Resolução dos exercícios 3 e 4

--- Lista02/listaExercicios02.js
+++ Lista02/listaExercicios02.js
@@ -1,10 +1,14 @@
-const prompt = require('prompt-sync')({ sigint: true });
+// const prompt = require('prompt-sync')({ sigint: true });
 
-// Lista de Exercícios 2
+/* 
+Lista de Exercícios 2
+---------------------------------------
 
-/* Seção 1: Estruturas de Controle Avançadas
+Seção 1: Estruturas de Controle Avançadas
+------------------------------------------------------------------------------
 
-01 - Validação de Datas:
+1 - Validação de Datas:
+---------------------------------------
 Crie a função ehDataValida(dia, mes, ano) que retorne true se os valores formarem  uma  data  real  (meses  de  28–31  dias,  ano  bissexto  para fevereiro)
 e false caso contrário. */
 
@@ -53,9 +57,11 @@ function ehDataValida(dia, mes, ano) {
   return true;
 }
 
-/*2. Jogo de Adivinhação
-// Escreva  um  script  que  gere um número aleatório de 1 a 100 e peça ao usuário,  para  adivinhar.
-// Use  while  para  repetir  até  acertar,  contando tentativas e exibindo “mais alto” ou “mais baixo” a cada palpite errado. */
+/*
+2 - Jogo de Adivinhação
+---------------------------------------
+Escreva  um  script  que  gere um número aleatório de 1 a 100 e peça ao usuário,  para  adivinhar.
+Use  while  para  repetir  até  acertar,  contando tentativas e exibindo “mais alto” ou “mais baixo” a cada palpite errado. */
 
 function guessNumber() {
   let userInput = 0;
@@ -89,21 +95,91 @@ function guessNumber() {
   console.log('Fim do jogo!');
 }
 
-/* 3. Palavras Únicas
+/* 
+3 - Palavras Únicas
+---------------------------------------
 Dada uma string (ex.: "olá olá mundo mundo"), use if/else e for para extrair todas as palavras únicas e exibi-las em um array. */
 
-// Seção 2: Funções e Recursão
+function singleWord(str) {
+  if (!isNaN(str)) {
+    console.log(
+      'Você digitou algum valor diferente de uma string.\nDigite um valor válido...',
+    );
+    return;
+  }
 
-/* 4. Fatorial Recursivo
+  let wordsArr = str.split(' ');
+  let singleWords = [];
+  let word = '';
+
+  for (let i = 0; i < wordsArr.length; i++) {
+    word = wordsArr[i];
+
+    if (!singleWords.includes(word)) {
+      singleWords.push(word);
+    }
+  }
+  return singleWords;
+}
+
+/* 
+Seção 2: Funções e Recursão
+------------------------------------------------------------------------------
+
+4 - Fatorial Recursivo
+---------------------------------------
 Implemente function fatorial(n) de forma recursiva; trate n < 0 lançando um Error, e n === 0 retornando 1. */
 
-// const executarExercicios = (nomeDaFuncao) => {
-//   typeof nomeDaFuncao == 'function'
-//     ? nomeDaFuncao()
-//     : console.log(
-//         'Você inseriu um nome inválido para a função que deseja executar!!\nInsira novamente o nome correto da função correspondente ao exercício desejado',
-//       );
-// };
-// qual é o erro?
+function fatorial(n) {
+  if (n < 0) {
+    throw new Error(
+      'Essa função não calcula o fatorial de um número negativo! \nEscolha um número positivo para o calculo.',
+    );
+  }
 
-// Digitar abaixo (entre os parênteses) nome da função que se quer executar sem os parênteses:
+  try {
+    if (n === 0) {
+      return 1;
+    } else {
+      return n * fatorial(n - 1);
+    }
+  } catch (error) {
+    console.log('Erro: ', error.messege);
+  }
+}
+console.log(fatorial(-5));
+
+/*
+5- Debounce​
+---------------------------------------
+Crie function debounce(fn, delay) que receba uma função fn e um delay em ms, retornando uma nova função que só executa fn se não for
+chamada novamente dentro do intervalo. */
+
+/*
+6. Memoization​
+---------------------------------------
+Implemente function memoize(fn) que armazene em cache chamadas anteriores de fn (por argumentos), retornando resultados instantâneos em
+repetidas invocações. */
+
+/*
+Seção 3: Arrays e Objetos Complexos
+------------------------------------------------------------------------------
+
+7. Mapeamento e Ordenação​
+---------------------------------------
+Dado um array produtos = [{ nome, preco }, …], crie uma função que retorne um novo array apenas com os nomes, 
+ordenados por preço crescente, usando map, sort. */
+
+/*
+8. Agrupamento por Propriedade​
+---------------------------------------
+Em vendas = [{ cliente, total }, …], use reduce para gerar um objeto onde cada chave é um cliente e o valor é a soma de todos os seus total. */
+
+/*
+9. Conversão Entre Formatos​
+---------------------------------------
+Escreva duas funções:
+1)​ paresParaObjeto(pares) recebe um array de pares [ [chave, valor], … ] e retorna o objeto equivalente.
+2) objetoParaPares(obj) faz o inverso, retornando um array de pares. */
+
+// Digitar abaixo nome da função que se quer executar:
